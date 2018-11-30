60cd0d54b1ef951eb3b33f7a51dbfca233afe957
Colin Smith <colin.smith@corfinancialgroup.com>
2018-11-30 12:05:25 +0000

    make simpleDisplay

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -27,43 +27,46 @@ export class UsersComponent implements OnInit {
             return -1;
           }
         });
-        const nDat = this.users.length;
-        const base = d3.select('app-users').append('svg')
-          .attr('width', 500)
-          .attr('height', (nDat + 2) * 20);
-        base.append('text')
-          .attr('x', 5)
-          .attr('y', 25)
-          .attr('transform', `translate(${10},${0})`)
-          .text(() => {
-            let back = '';
-            Object.keys(this.users[0]).forEach((k) => back += `${k} `);
-            return back;
-          })
-          .attr('class', 'users');
-        base.append('rect')
-          .attr('class', 'users')
-          .attr('width', 490)
-          .attr('height', 24)
-          .attr('x', 5)
-          .attr('y', 3);
-        base.selectAll('inner').data(this.users).enter().append('text')
-          .attr('x', 5)
-          .attr('y', 52)
-          .attr('transform', (d, i) => `translate(${10},${i * 20})`)
-          .text((d) => {
-            let back = '';
-            Object.keys(d).forEach((k) => back += `${d[k]} `);
-            return back;
-          })
-          .attr('class', 'users');
-        base.append('rect')
-          .attr('class', 'users')
-          .attr('width', 490)
-          .attr('height', nDat * 21)
-          .attr('x', 5)
-          .attr('y', 32);
+        this.simpleDisplay();
       });
   }
 
+  simpleDisplay() {
+    const nDat = this.users.length,
+      base = d3.select('app-users').append('svg')
+        .attr('width', 500)
+        .attr('height', (nDat + 2) * 20);
+    base.append('text')
+      .attr('x', 5)
+      .attr('y', 25)
+      .attr('transform', `translate(${10},${0})`)
+      .text(() => {
+        let back = '';
+        Object.keys(this.users[0]).forEach((k) => back += `${k} `);
+        return back;
+      })
+      .attr('class', 'users');
+    base.append('rect')
+      .attr('class', 'users')
+      .attr('width', 490)
+      .attr('height', 24)
+      .attr('x', 5)
+      .attr('y', 3);
+    base.selectAll('inner').data(this.users).enter().append('text')
+      .attr('x', 5)
+      .attr('y', 52)
+      .attr('transform', (d, i) => `translate(${10},${i * 20})`)
+      .text((d) => {
+        let back = '';
+        Object.keys(d).forEach((k) => back += `${d[k]} `);
+        return back;
+      })
+      .attr('class', 'users');
+    base.append('rect')
+      .attr('class', 'users')
+      .attr('width', 490)
+      .attr('height', nDat * 21)
+      .attr('x', 5)
+      .attr('y', 32);
+  }
 }
