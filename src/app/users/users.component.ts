1217d40f1a3cca75b8933764f698edb9ec3c1eae
Colin Smith <colin.smith@corfinancialgroup.com>
2018-11-30 11:24:52 +0000

    Only d3

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -27,15 +27,42 @@ export class UsersComponent implements OnInit {
             return -1;
           }
         });
+        const nDat = this.users.length;
         const base = d3.select('app-users').append('svg')
           .attr('width', 500)
-          .attr('height', 500);
-        base.selectAll('inner').data(this.users).enter().append('text')
-          .attr('x', 0)
-          .attr('y', 0)
-          .attr('transform', (d, i) => `translate(${20},${i * 30 + 30})`)
-          .text((d) => `${d.id} ${d.movies} ${d.name}`)
+          .attr('height', (nDat + 2) * 20);
+        base.append('text')
+          .attr('x', 5)
+          .attr('y', 25)
+          .attr('transform', `translate(${10},${0})`)
+          .text(() => {
+            let back = '';
+            Object.keys(this.users[0]).forEach((k) => back += `${k} `);
+            return back;
+          })
           .attr('class', 'users');
+        base.append('rect')
+          .attr('class', 'users')
+          .attr('width', 490)
+          .attr('height', 24)
+          .attr('x', 5)
+          .attr('y', 3);
+        base.selectAll('inner').data(this.users).enter().append('text')
+          .attr('x', 5)
+          .attr('y', 52)
+          .attr('transform', (d, i) => `translate(${10},${i * 20})`)
+          .text((d) => {
+            let back = '';
+            Object.keys(d).forEach((k) => back += `${d[k]} `);
+            return back;
+          })
+          .attr('class', 'users');
+        base.append('rect')
+          .attr('class', 'users')
+          .attr('width', 490)
+          .attr('height', nDat * 21)
+          .attr('x', 5)
+          .attr('y', 32);
       });
   }
 
