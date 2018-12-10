8a36b6396db2943a182aaa37cc3d1370c7cfb9cb
Colin Smith <colin.smith@corfinancialgroup.com>
2018-12-10 10:21:05 +0000

    Get rid of unnecessary things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsersComponent } from './users/users.component';
 import { UserService } from './users/user.service';
-import {APP_BASE_HREF} from '@angular/common';
-
-const routes: Routes = [
-  { path: 'router', component: UsersComponent },
-  { path: 'dashboard', component: DashboardComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     UsersComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
     HttpClientModule,
   ],
-  exports: [
-    RouterModule
-  ],
-  providers: [UserService, {provide: APP_BASE_HREF, useValue : '/' }],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
