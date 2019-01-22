b89a71e77a456748792a6bffdb9224c2f37274ad
Colin Smith <colin.smith@corfinancialgroup.com>
2019-01-22 15:05:15 +0000

    forgot

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Cache-Control", "no-store");
+  res.header("Cache-Control", "max-age=6");
   next();
 });
 
