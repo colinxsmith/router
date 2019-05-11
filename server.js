d4cc87a23ab386d81ec5fe15fdca3a73df4b5542
Colin <colinxsmith@gmail.com>
2019-05-11 17:04:50 +0100

    forgot

--- server.js
+++ server.js
@@ -22,21 +22,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Set our api routes
 app.use('/', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 /**
  * Get port from environment and store in Express.
  */
 const port = process.env.PORT || '4024';
-const host = process.env.HOST || '10.2.70.36';
+const host = process.env.HOST || '192.168.0.27';
 
 app.set('port', port);
 app.set('host', host);
